9a5ab551c1ab3aa7c91a7a6fa70ecec3f619d6a7
Daniel Moran Vilchez <113327439+DanielMoranV@users.noreply.github.com>
2025-05-25 14:01:57 -0500

    configuracion de estructura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompaniesModule } from './companies/companies.module';
+import { UsersModule } from './users/users.module';
+import { UnitsModule } from './units/units.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [CompaniesModule, UsersModule, UnitsModule, ProductsModule, CategoriesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
